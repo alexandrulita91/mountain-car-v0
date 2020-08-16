eccef8afabf540e6264bd029d0efa92b46df42ca
Trusty Coder <trustycoder83@gmail.com>
2020-08-17 00:59:59 +0300

    Update dqn_mountain_car.py

--- dqn_mountain_car.py
+++ dqn_mountain_car.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     # Defines training related constants
     batch_size = 32
     num_episodes = 1000
-    num_episode_steps = 200  # constant value
+    num_episode_steps = env.spec.max_episode_steps  # constant value
     action_size = env.action_space.n
     state_size = env.observation_space.shape[0]
 
