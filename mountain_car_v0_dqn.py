5756847d5ad2439b6e89dc4767643dd9e3569ada
Trusty Coder <trustycoder83@gmail.com>
2020-09-09 01:07:30 +0300

    Update mountain_car_v0_dqn.py

--- mountain_car_v0_dqn.py
+++ mountain_car_v0_dqn.py
@@ -22,7 +22,7 @@ class Agent:
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.learning_rate = 0.0005
         self.model = self._build_model()
 
     def _build_model(self):
